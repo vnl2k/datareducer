ec5924304a3a853dbe3e18f4be60e22bcf8b510d
vnl2k <38137627+vnl2k@users.noreply.github.com>
2018-08-06 15:55:43 +0100

    Made the package available on Test PyPI

--- build-pypi.py
+++ build-pypi.py
@@ -3,9 +3,9 @@ from shutil import rmtree, move
 
 PACKAGE_NAME = "datareducer"
 
-os.system("python3 setup.py sdist bdist_wheel")
+os.system("python3 setup.py sdist")
 rmtree("./" + PACKAGE_NAME + ".egg-info")
-rmtree("./build")
+# rmtree("./build")
 
 # test.pypi.org
 os.system('python3 -m twine upload --repository-url "https://test.pypi.org/legacy/" "dist/*"')
--- build.py
+++ build.py
@@ -3,7 +3,7 @@ import os
 from shutil import rmtree, move
 from glob import glob
 
-PACKAGE_NAME = "datashader"
+PACKAGE_NAME = "datareducer"
 
 os.system("python3 setup.py sdist")
 rmtree("./" + PACKAGE_NAME + ".egg-info")
